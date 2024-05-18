3f57561ab3824b93416a9866d65175d5617cf5ea
ITN\itnam <itnams12@gmail.com>
2024-05-18 11:18:20 +0700

    fix(home): padding home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { NavigationBarComponent } from '@detect-license-plate/navigation-bar';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, NavigationBarComponent],
+  imports: [ RouterModule, NavigationBarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
